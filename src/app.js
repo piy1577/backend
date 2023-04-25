aab88ec47e91f3f8b8a50da4cf18b21bf7af8e35
Piyush@laptop <chandra7piyush@gmail.com>
2023-04-25 23:35:41 +0530

    origin set

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 import express from "express";
 import { getAllUser } from "./User.model.js";
+import cors from "cors";
 const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 app.get("/", async (req, res) => {
     res.status(200).json(await getAllUser());
