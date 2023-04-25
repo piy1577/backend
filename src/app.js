b6d27999549a0afafe36e14f31b0339f3d068887
Piyush@laptop <chandra7piyush@gmail.com>
2023-04-25 23:28:21 +0530

    backend change

--- src/app.js
+++ src/app.js
@@ -1,13 +1,6 @@
 import express from "express";
 import { getAllUser } from "./User.model.js";
-import cors from "cors";
 const app = express();
-
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-    })
-);
 
 app.get("/", async (req, res) => {
     res.status(200).json(await getAllUser());
